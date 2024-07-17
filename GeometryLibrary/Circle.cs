8a0e89e0060c390aa2499b6821da6a2b64c1ffdf
Dzmitry Hastsilovich <dzmitryhastsilovich@MacBook-Pro-Dim.local>
2024-07-17 15:29:23 +0300

    Base commit

---
+++ GeometryLibrary/Circle.cs
@@ -0,0 +1,25 @@
+﻿namespace GeometryLibrary;
+
+public class Circle : IShape
+{
+    private readonly double _radius;
+
+    public Circle(double radius)
+    {
+        CheckRadiusValidity(radius);
+        _radius = radius;
+    }
+
+    public double CalculateArea()
+    {
+        return Math.PI * Math.Pow(_radius, 2);
+    }
+
+    private void CheckRadiusValidity(double radius)
+    {
+        if (radius <= 0)
+        {
+            throw new ArgumentException("Radius must be positive", nameof(radius));
+        }
+    }
+}
